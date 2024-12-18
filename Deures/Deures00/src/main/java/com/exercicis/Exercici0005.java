5c5015e9bbf67f8103ec741bf27d602a526d1575
victor06-git <asensiovector@gmail.com>
2024-12-18 11:05:56 +0100

    .

--- Deures/Deures00/src/main/java/com/exercicis/Exercici0005.java
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0005.java
@@ -3,35 +3,42 @@ package com.exercicis;
 public class Exercici0005 {
 
     public static boolean isPalindrom(String text) {
-        
-        String newtext = text.toLowerCase().replace(" ", "").replace("·", "").replace("'", ""); //Treu tots els espais i ajuntar tots els caràcters amb un sol string
-        
-        for(char c: newtext.toCharArray()){
-            //Crear string a partir de array invertido para palindrom
-            String inverted = "Hola";
-        }
-        String inverted = "Hola";
-        
-        if (newtext = inverted){
-            System.out.println("True");
-        } else {
-            System.out.println("False");
-        }
 
-        return false;
+        text = normalize(text);
+        String inverted = ""; //Creació String invertido
+        
+        for(int i = text.length() - 1; i >= 0; i--) {
+            //Crear string invertido a partir de string para palindrom
+            inverted += text.charAt(i);
+        }
+        return text.equals(inverted);
     }
 
     public static String normalize(String text) {
         
-        String resultat  = "Si";
-        System.out.printf("'%s' (%s)", text, resultat);
+        String newtext = text.toLowerCase().replace(" ", "").replace("·", "").replace("'", ""); //Treu tots els espais i ajuntar tots els caràcters amb un sol string
         
-        return "";
+        return newtext;
     }
    
     public static void main(String[] args) {
         
-        String text = "Anul·la la lluna";
+        String[] frases ={
+            "Anul·la la lluna",
+            "Atrapa la lluna",
+            "Atrapa'l o l'aparta",
+            "Aparta'l o atrapa'l",
+            "No sap pas on",
+            "On sap pas qui",
+            "Tramaran anar a Mart",
+            "A Mart trobaràn art",
+            "Un pop nu",
+            "Nu pop un"
+        };
 
+        for (String f: frases) {
+            boolean esPalindrom = isPalindrom(f);
+            System.out.println(f + " (" + (esPalindrom ? "Si" : "No") + ")"); //Print de las frases + si són Palindrom
+        }
     }
 }
