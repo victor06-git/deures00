ff515375dea867c73860c843d2a6d533beb20d30
victor06-git <asensiovector@gmail.com>
2024-12-18 12:46:09 +0100

    Exercici0006

--- Deures/Deures00/src/main/java/com/exercicis/Exercici0006.java
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0006.java
@@ -5,17 +5,30 @@ import java.util.Locale;
 public class Exercici0006 {
 
     public static double calculaEntrada(int numeroAdults, int numNens, int numGrans, String dia) {
-        /*
-            TODO: Resol aquí la funció
-        */
+        
+        double entradaAdults = 10.0;
+        double entradaNens = 5.0;
+        double entradaGrans = 6.0;
+
+        double entradaTotal = (numeroAdults * entradaAdults) + (numNens * entradaNens) + (numGrans * entradaGrans);
+
+        if(dia.equalsIgnoreCase("dimarts")){
+            entradaTotal = entradaTotal - (entradaTotal * 0.2);
+        }
+
+        if(dia.equalsIgnoreCase("dijous") && numNens > 0){
+            if(numeroAdults >= 1){
+                entradaTotal -= entradaNens * 0.5  //Paga la meitat del preu del nen
+                entradaTotal -= entradaNens * (numNens - 1); //Paga només un nen
+            
+            }
+        }
 
         return 0;
     }
 
     public static void main(String[] args) {
-
-        /*
-            TODO: Resol aquí l'exercici
-        */
+        
+        System.out.printf(LocaleUS, )
     }
 }
