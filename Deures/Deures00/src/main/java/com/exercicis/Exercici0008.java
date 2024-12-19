51a043b87d636ed563c2eab44df19c94d0375475
victor06-git <asensiovector@gmail.com>
2024-12-19 16:25:45 +0100

    Exercici0008

--- Deures/Deures00/src/main/java/com/exercicis/Exercici0008.java
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0008.java
@@ -8,9 +8,9 @@ public class Exercici0008 {
     public static ArrayList<Integer> generaImparells(int numero) {
         ArrayList<Integer> imparells = new ArrayList<>();
         
-        /*
-            TODO: Resol aquí la funció
-        */
+        for (int i = 3; i <= numero; i+= 2) {
+            imparells.add(i);
+        }
         
         return imparells;
     }
@@ -18,9 +18,17 @@ public class Exercici0008 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        /*
-            TODO: Resol aquí l'exercici
-        */
+        System.out.print("Introdueix un número: ");
+        int num_1 = scanner.nextInt();
+
+        
+
+        if (num_1 <= 2){
+            System.out.println("No hi ha nombres imparells entre 2 i " + num_1);
+        } else {
+            ArrayList<Integer> imparells = generaImparells(num_1);
+            System.out.println("Nombres imparells entre 2 i " + num_1 + ": " + imparells);
+        }
 
         scanner.close();
     }
